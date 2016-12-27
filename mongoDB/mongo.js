8e21c1b758906181f215f9282c0a4dfa279453da
TedtheDev <tylerjedmunds@gmail.com>
2016-12-27 16:48:37 -0600

    added mongo test connection with es6

---
+++ mongoDB/mongo.js
@@ -0,0 +1,33 @@
+//set strict mode for es6
+'use strict';
+
+//require MongoDB
+const mongodb = require('mongodb');
+
+//Load the MongoClient to connect
+const MongoClient = mongodb.MongoClient;
+
+//decalre URL
+let fullURL;
+
+//Mongo Connection attributes
+const mongoDBUrl = 'mongodb';
+const hostWithPort = 'localhost:27017';
+const databaseName = 'test';
+
+//construct fullURL
+fullURL = mongoDBUrl + '://' + hostWithPort +'/' + databaseName;
+
+//Connect to DB and do some work
+MongoClient.connect(fullURL, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  }
+  else {
+    console.log('Connection established to', fullURL);
+
+    //do some work
+
+    db.close();
+  }
+})
