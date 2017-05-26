57d28b75abba85bd0ee39aa3ca27667d07e6af25
TedtheDev <tylerjedmunds@gmail.com>
2017-05-26 00:00:30 -0500

    added tic tac toe board logic to place X and O, added computer logic to place randomly

--- TicTacToeAppWithRedux/src/containers/gameboard.js
+++ TicTacToeAppWithRedux/src/containers/gameboard.js
@@ -3,32 +3,61 @@ import { connect } from 'react-redux';
 import { } from '../actions/index';
 import { bindActionCreators } from 'redux';
 import SvgIcon from 'material-ui/SvgIcon';
-import GameBoardO from './gameboardO';
+import GameBoardBox from './gameboardbox';
 
 class GameBoard extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { col1row1: false}
-    this.placeX = this.placeX.bind(this);
+    this.state = {board: [['','',''],['','',''],['','','']], player: 'X'};
+
+    this.play = this.play.bind(this);
+    this.playComputer = this.playComputer.bind(this);
+    this.checkForWinner = this.checkForWinner.bind(this);
   }
 
-  placeX(event){
+  playComputer(newBoard) {
+    let generate = true;
+    let counter = 0
+    while(generate && counter < 1000) {
+      const randomRow = Math.floor(Math.random() * (1000 - 0)) % 3;
+      const randomBox = Math.floor(Math.random() * (1000 - 0)) % 3;
+      if(newBoard[randomRow][randomBox] === '') {
+        newBoard[randomRow][randomBox] = 'O';
+        generate = false;
+      }
+      counter++;
+    }
+  }
+
+  checkForWinner(newBoard) {
+
+  }
+
+  play(index, player){
+    index = index.split(',')
+    let newBoard = this.state.board;
+    newBoard[index[0]][index[1]] = player;
+    this.playComputer(newBoard);
+    this.setState({board: newBoard})
+    checkForWinner(this.state.board);
+    //const updatePlayer = this.state.player === 'X' ? 'O' : 'X';
+    //this.setState({player: updatePlayer});
   }
 
   render() {
     return (
       <div style={{height:"100%"}}>
         <div className='gameboard'>
-          <GameBoardO className='col-1 row-1' onClick={this.place} />
-          <GameBoardO className='col-1 row-2' onClick={this.place} />
-          <GameBoardO className='col-1 row-3' onClick={this.place} />
-          <GameBoardO className='col-2 row-1' onClick={this.place} />
-          <GameBoardO className='col-2 row-2' onClick={this.place} />
-          <GameBoardO className='col-2 row-3' onClick={this.place} />
-          <GameBoardO className='col-3 row-1' onClick={this.place} />
-          <GameBoardO className='col-3 row-2' onClick={this.place} />
-          <GameBoardO className='col-3 row-3' onClick={this.place}  />
+          <GameBoardBox box={this.state.board[0][0]} index='0,0' player={this.state.player} play={this.play} className='col-1 row-1' />
+          <GameBoardBox box={this.state.board[0][1]} index='0,1' player={this.state.player} play={this.play} className='col-1 row-2' />
+          <GameBoardBox box={this.state.board[0][2]} index='0,2' player={this.state.player} play={this.play} className='col-1 row-3' />
+          <GameBoardBox box={this.state.board[1][0]} index='1,0' player={this.state.player} play={this.play} className='col-2 row-1' />
+          <GameBoardBox box={this.state.board[1][1]} index='1,1' player={this.state.player} play={this.play} className='col-2 row-2' />
+          <GameBoardBox box={this.state.board[1][2]} index='1,2' player={this.state.player} play={this.play} className='col-2 row-3' />
+          <GameBoardBox box={this.state.board[2][0]} index='2,0' player={this.state.player} play={this.play} className='col-3 row-1' />
+          <GameBoardBox box={this.state.board[2][1]} index='2,1' player={this.state.player} play={this.play} className='col-3 row-2' />
+          <GameBoardBox box={this.state.board[2][2]} index='2,2' player={this.state.player} play={this.play} className='col-3 row-3' />
         </div>
       </div>
     )
