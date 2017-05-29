44635b7817012a37c28571e0d3ba0e24da9a734e
TedtheDev <tylerjedmunds@gmail.com>
2017-05-29 18:04:08 -0500

    reconfigured webpack, reconfigured index.html to account for bundles, setup scss directory -will need to refactor later

--- TicTacToeAppWithRedux/src/index.js
+++ TicTacToeAppWithRedux/src/index.js
@@ -11,7 +11,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 const createStoreWithMiddleware = applyMiddleware(
--- TicTacToeAppWithRedux/webpack.config.js
+++ TicTacToeAppWithRedux/webpack.config.js
@@ -1,26 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractCSS = new ExtractTextPlugin('bundle.css');
+
+const extractCommons = new webpack.optimize.CommonsChunkPlugin({
+  name: 'commons.js',
+  filename: 'commons.js'
+})
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ['./src/index.js', './style/scss/styles.scss'],
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/dist/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules:[
+      {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ["react", "es2015", "stage-1"]
+          }
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: extractCSS.extract({
+          use: [{
+                loader: "css-loader"
+              },{
+                loader: "sass-loader"
+              }]
+        })
       }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+    ]
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+    extractCSS
+  ]
 };
