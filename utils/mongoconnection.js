fe3fee8c5b4f72caf8aa3e20b3d228e7ffa61705
TedtheDev <tylerjedmunds@gmail.com>
2017-06-20 21:06:08 -0500

    prettified mongoconnection, testing process.env.PORT for production

--- utils/mongoconnection.js
+++ utils/mongoconnection.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
-const startDB = (database) => {
-  if(!process.env.MONGODB_URI) {
-    const creds = require('../creds/creds');
-    if(database === 'tic-tac-toe')
-      mongoose.connect(`mongodb://${creds.user}:${creds.pwd}@localhost/${database}?authSource=${creds.authSource}`);
-    mongoose.connection
-      .once('open', () => console.log('MongoDB Connected') )
-      .on('error', (error) => {
-        console.warn('Warning', error);
-      });
-  } else {
-    mongoose.connect(process.env.MONGODB_URI);
-  }
-}
+const startDB = database => {
+	if (!process.env.MONGODB_URI) {
+		const creds = require('../creds/creds');
+		if (database === 'tic-tac-toe')
+			mongoose.connect(
+				`mongodb://${creds.user}:${creds.pwd}@localhost/${database}?authSource=${creds.authSource}`
+			);
+		mongoose.connection
+			.once('open', () => console.log('MongoDB Connected'))
+			.on('error', error => {
+				console.warn('Warning', error);
+			});
+	} else {
+		mongoose.connect(process.env.MONGODB_URI);
+	}
+};
 
 module.exports = startDB;
