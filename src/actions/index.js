c1fdb8475387d726a2d62a126cc129cdd89ea816
TedtheDev <tylerjedmunds@gmail.com>
2017-06-25 20:48:43 -0500

    added comments and documentation to action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,53 @@
 import axios from 'axios';
 
+// Chat Message Types
 export const FETCH_CHAT_MESSAGES = 'FETCH_CHAT_MESSAGES';
 export const DELETE_CHAT_MESSAGE = 'DELETE_CHAT_MESSAGE';
 export const CREATE_CHAT_MESSAGE = 'CREATE_CHAT_MESSAGE';
 
+// Login/logout Types
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 
+// Create Account/Player Types
 export const CREATING_PLAYER = 'CREATING_PLAYER';
 export const CREATED_PLAYER_ERROR = 'CREATED_PLAYER_ERROR';
 export const CREATED_PLAYER_SUCCESS = 'CREATED_PLAYER_SUCCESS';
 export const CREATED_PLAYER_CHANGE = 'CREATED_PLAYER_CHANGE';
 
-
+// Update Player/Account Types
+// update player/account info
 export const UPDATE_PLAYER = 'UPDATE_PLAYER';
 export const UPDATING_PLAYER = 'UPDATING_PLAYER';
 export const UPDATE_PLAYER_ERROR = 'UPDATE_PLAYER_ERROR';
 export const UPDATE_PLAYER_SUCCESS = 'UPDATE_PLAYER_SUCCESS';
 export const SET_PLAYER_INFO = 'SET_PLAYER_INFO';
+// Update player/account wins, draws, loses
+export const UPDATE_WINS = 'UPDATE_WINS';
+export const UPDATE_LOSES = 'UPDATE_LOSES';
+export const UPDATE_DRAWS = 'UPDATE_DRAWS';
 
+// Root url for api
+// for now when deploying to prod
+// (yes i know that's not good, need to research a better way)
+// switch to ./api
+// prod = ./api
+// dev = http://localhost:3050/api
 const ROOT_URL = 'http://localhost:3050/api';
 
+
+// **********************************************************************************************
+// Action Creators for creating, fetching, and deleting a message related to one player
+// **********************************************************************************************
+/**
+ * [fetchChatMessages - fetch messages based on username]
+ * @param  {[string]} username [username related to player]
+ * @return {[action]} action [redux action]
+ */
 export function fetchChatMessages(username) {``
   const token = localStorage.getItem('token');
   const request = axios.get(`${ROOT_URL}/chatsystem/messages/${username}?token=${token}`);
@@ -35,6 +57,12 @@ export function fetchChatMessages(username) {``
   };
 }
 
+/**
+ * [deleteChatMessage delete message based on username and ID of message]
+ * @param  {[string]} username [username related to player]
+ * @param  {[string]} id       [ID of message]
+ * @return {[type]}          [redux action]
+ */
 export function deleteChatMessage(username, id) {
   const token = localStorage.getItem('token');
   const request = axios.delete(`${ROOT_URL}/chatsystem/messages/${username}/${id}?token=${token}`);
@@ -44,6 +72,12 @@ export function deleteChatMessage(username, id) {
   }
 }
 
+/**
+ * [createChatMessage - create a message related to username and the message text to store]
+ * @param  {[string]} username [username related to player]
+ * @param  {[string]} message  [message text to save]
+ * @return {[type]}          [redux action]
+ */
 export function createChatMessage(username, message) {
   const token = localStorage.getItem('token');
   const reqBody = { user: username, message: message, token: token };
@@ -55,8 +89,13 @@ export function createChatMessage(username, message) {
 }
 
 
-// handle account creation
-// ----
+// **********************************************************************************************
+// Action Creators for creating a player
+// **********************************************************************************************
+/**
+ * [creatingPlayer - redux action to update isCreating to true to render loading icon]
+ * @return {[type]} [redux action]
+ */
 function creatingPlayer() {
   return {
     type: CREATING_PLAYER,
@@ -64,6 +103,11 @@ function creatingPlayer() {
   }
 }
 
+/**
+ * [createdPlayerSuccess - action to return player created and update iscreating and created state]
+ * @param  {[type]} player [information related to person playing on the site]
+ * @return {[type]}        [redux action]
+ */
 function createdPlayerSuccess(player) {
   return {
     type: CREATED_PLAYER_CHANGE,
@@ -71,6 +115,11 @@ function createdPlayerSuccess(player) {
   }
 }
 
+/**
+ * [createdPlayerError - throw error message on error while creating a player]
+ * @param  {[string]} errorMessage [error message text to show on screen]
+ * @return {[type]}              [redux action]
+ */
 function createdPlayerError(errorMessage) {
   return {
     type: CREATED_PLAYER_ERROR,
@@ -78,9 +127,14 @@ function createdPlayerError(errorMessage) {
   }
 }
 
+/**
+ * [createPlayer - main function to initiate creating a player]
+ * @param  {[player]} player [contains info related to account and player information]
+ * @return {[type]}        [redux action]
+ */
 export function createPlayer(player) {
-  const { firstname, lastname, password, username, email } = player;
-  const reqBody = { firstname, lastname, password, username, email}
+  const { password, username, email } = player;
+  const reqBody = { password, username, email}
   return (dispatch) => {
     dispatch(creatingPlayer());
     return axios.post(`${ROOT_URL}/account/create`, reqBody)
@@ -107,7 +161,7 @@ export function createPlayer(player) {
 // to handle data and state
 // ****************************************************************************
 /**
- * [updatingPlayer - simple action creator to initiate loading]
+ * [updatingPlayer - simple action creator to initiate loading when updating]
  * @return {[action]} [description]
  */
 function updatingPlayer() {
@@ -131,7 +185,8 @@ function updatePlayerError(errorMessage) {
 }
 
 /**
- * [updatePlayerSuccess - ]
+ * [updatePlayerSuccess - update account state and pass player info to state
+ * when updating info is a success]
  * @param  {[player]}  [player info of person playing the game]
  * @return {[action]}  [redux action]
  */
@@ -143,7 +198,7 @@ function updatePlayerSuccess(player) {
 }
 
 /**
- * [updatePlayer updates a player's account information]
+ * [updatePlayer - updates a player's account information]
  * @param  {[player]}  [player that is playing the game]
  * @return {[action]}  [redux action]
  */
@@ -176,6 +231,11 @@ export function updatePlayer(player) {
   }
 }
 
+/**
+ * [setPlayerInfo - action creator to update account state from authentication state]
+ * @param {[object]} player [player info]
+ * @return {[type]}       [redux action]
+ */
 function setPlayerInfo(player) {
   return {
     type: SET_PLAYER_INFO,
@@ -184,9 +244,15 @@ function setPlayerInfo(player) {
 }
 
 
-// ------------------------------
-// to handle user authentication
-// used in loginUser action creator below
+// ******************************************************************************
+// Action creators for logging in a player and handling errors
+// ******************************************************************************
+/**
+ * [requestLogin - set isFetching to true to rending loading icon
+ * and pass creds to state]
+ * @param  {[object]} creds [password and username of player wrapped in an object]
+ * @return {[type]}       [redux action]
+ */
 function requestLogin(creds) {
   return {
     type: LOGIN_REQUEST,
@@ -196,6 +262,11 @@ function requestLogin(creds) {
   }
 }
 
+/**
+ * [receiveLogin - ]
+ * @param  {[object]} player [player info]
+ * @return {[type]}        [redux action]
+ */
 function receiveLogin(player) {
   return {
     type: LOGIN_SUCCESS,
@@ -206,6 +277,11 @@ function receiveLogin(player) {
   }
 }
 
+/**
+ * [loginError - throw login error and set errorMessage for onscreen]
+ * @param  {[string]} message [error message for user]
+ * @return {[type]}         [redux action]
+ */
 function loginError(message) {
   const payload = { isFetching: false, isAuthenticated: false, errorMessage: message };
   return {
@@ -216,8 +292,11 @@ function loginError(message) {
 }
 // ----------------------------------
 
-// --------
-// handle logging in user
+/**
+ * [loginPlayer - main function to initiate logging in a player]
+ * @param  {[object of strings]} creds [player password and username]
+ * @return {[type]}       [redux action]
+ */
 export function loginPlayer(creds) {
   const reqBody = { username: creds.username, password: creds.password };
   const config = { headers: { "Content-Type": "application/json" } };
@@ -243,6 +322,13 @@ export function loginPlayer(creds) {
 }
 
 
+//***********************************************************************************
+// Action Creators for logging out a player
+//***********************************************************************************
+/**
+ * [requestLogout - update isFetching state when logging out]
+ * @return {[type]} [redux action]
+ */
 function requestLogout() {
   return {
     type: LOGOUT_REQUEST,
@@ -252,6 +338,11 @@ function requestLogout() {
   }
 }
 
+/**
+ * [receiveLogout - update isFetching and isAuthenticated to false to
+ * reset login/logout state]
+ * @return {[type]} [redux action]
+ */
 function receiveLogout() {
   return {
     type: LOGOUT_SUCCESS,
@@ -261,7 +352,11 @@ function receiveLogout() {
   }
 }
 
-// Logs the user out
+/**
+ * [logoutUser - main function that logs out user by requesting
+ * logout, removing token, and updating isFetching and isAuthenticated state]
+ * @return {[type]} [redux dispatch]
+ */
 export function logoutUser() {
   return (dispatch) => {
     dispatch(requestLogout())
