1d4e66d6240424de98ed0477b1c419fb1ea56d15
TedtheDev <tylerjedmunds@gmail.com>
2017-06-29 23:06:54 -0500

    added comments for leaderboards layout, fixed update account route

--- controllers/account_controllers.js
+++ controllers/account_controllers.js
@@ -37,7 +37,9 @@ module.exports = {
         if(!player) {
           res.json({ success: false, message: "Username incorrect"});
         } else if(player){
-          if(body.oldPassword !== null && body.newPassword !== null) {
+          console.log(body)
+          if(body.oldPassword !== null && body.oldPassword.length > 0
+            && body.newPassword !== null && body.newPassword.length > 0) {
             bcrypt.compare(req.body.oldPassword, player.password, (err, response) => {
               if(!response) {
                 res.json({ success: false, message: 'Old Password incorrect'});
--- src/containers/leaderboard.js
+++ src/containers/leaderboard.js
@@ -4,6 +4,11 @@ class Leaderboard extends Component {
     render() {
       return (
         <div>Leaderboards coming soon</div>
+        // get all players. show games played, wins, win rate, fun stats
+        // do a list, just a one call
+        // will eventually make it sortabled maybe with a search?
+        // do it ascending on win rate, but somehow incorporate games played too
+        // include player logged in like in pubg
       )
     }
 }
