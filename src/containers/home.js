8b791d5fb283e54f25f53e7c8b2c1e7081f02001
TedtheDev <tylerjedmunds@gmail.com>
2017-07-08 21:11:02 -0500

    added comments to change host url's

--- src/containers/home.js
+++ src/containers/home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import LoginScreen from './login';
 import { Link, Route } from 'react-router-dom';
 import io from 'socket.io-client';
+// production = change to ()
+// dev = io('http://localhost:3050/')
 const socket = io('http://localhost:3050/');
 import RaisedButton from 'material-ui/RaisedButton';
 
