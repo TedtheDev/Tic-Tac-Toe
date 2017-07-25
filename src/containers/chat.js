0f866b40392ab130636a9c57fb0d59e9d5ba041d
TedtheDev <tylerjedmunds@gmail.com>
2017-07-25 00:06:30 -0500

    fixed leaderboard layout a bit and styled buttons for pagination, added some socket io to chat message

--- src/containers/chat.js
+++ src/containers/chat.js
@@ -5,6 +5,9 @@ import { bindActionCreators } from 'redux';
 import moment from 'moment';
 import CreateChatMessage from './create_chat_message';
 import LoadingIcon from '../components/loading_material_ui_circular';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3050/')
 
 class Chat extends Component {
   constructor(props) {
