ea4125fa0fa2a95217cb3bdc3281ae524aa2a265
TedtheDev <tylerjedmunds@gmail.com>
2017-07-27 23:11:04 -0500

    UI added for forgot password/reset your password

--- src/components/forgot_password.js
+++ src/components/forgot_password.js
@@ -4,29 +4,41 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Field } from 'redux-form';
 
-const renderTextField = ({input, label, theType, meta: {touched, error}, onInputChange, theValue, ...custom}) => {
+const renderTextField = ({input, emailValue, label, meta: {touched, error}, onInputChangeEmail}) => {
+    input.onChange = onInputChangeEmail;
+    input.value = emailValue;
     return (
       <TextField
+        {...input}
         type='text'
-        floatingLabelText="Email"
+        floatingLabelText={label}
         floatingLabelStyle={{color:"#000000"}}
+        errorText={touched && error}
         underlineStyle={{color:"#000000"}}
+        onChange={onInputChangeEmail}
+        value={emailValue}
         autoComplete="off"
         style={{width:"90%"}}
       />
     );
 }
 
-const ForgotPassword = ({ values, handleSubmit, onSubmitResetPassword }) => {
+const ForgotPassword = (props) => {
   return (
-    <form onSumbit={handleSubmit(onSubmitResetPassword)}>
+    <form onSubmit={props.handleSubmit(props.onSubmitResetPassword)}>
       <Paper zDepth={5} style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
         <h2>Reset Your Password</h2>
-        <Field name='email' type='text' label='Email' component={renderTextField} />
+        <Field
+          name='email'
+          label='Email'
+          component={renderTextField}
+          onInputChangeEmail={props.onInputChangeEmail}
+          emailValue={props.emailValue}
+        />
         <RaisedButton
           label='Reset Password'
           primary={true}
-          style={{}}
+          style={{marginTop:"10%"}}
           type='submit'
         />
       </Paper>
--- src/containers/forgot_password_wrapper.js
+++ src/containers/forgot_password_wrapper.js
@@ -1,38 +1,48 @@
 import React, { Component } from 'react';
 import ForgotPassword from '../components/forgot_password';
-import { reduxForm, Field } from 'redux-form';
-
-const validate = (values, props) => {
-  const errors = {};
-  if(!values['email'] && values['email'] === '') {
-    errors['email'] = 'Required';
-  } else if(values['email'].length > 0 && values['email'].indexOf('@') === -1) {
-    errors['email'] = 'Invalid Email: use @'
-  }
-  return errors;
-}
+import { reduxForm } from 'redux-form';
 
 class ForgotPasswordWrapper extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {email: ''};
     this.onSubmitResetPassword = this.onSubmitResetPassword.bind(this);
+    this.onInputChangeEmail = this.onInputChangeEmail.bind(this);
+  }
+
+  onInputChangeEmail(event) {
+    this.setState({email: event.target.value})
   }
 
   onSubmitResetPassword() {
     // axios call to reset pass
+    console.log('onSubmitResetPassword', this.state.email)
   }
 
   render() {
     return (
       <div>
         <ForgotPassword
-          values={values}
+          handleSubmit={this.props.handleSubmit}
           onSubmitResetPassword={this.onSubmitResetPassword}
+          onInputChangeEmail={this.onInputChangeEmail}
+          emailValue={this.state.email}
+          {...this.props}
         />
       </div>
     )
   }
+}
+
+const validate = (values, props) => {
+  const errors = {};
+  values.email = values.email || '';
+  if(!values.email && values.email === '') {
+    errors.email = 'Required';
+  } else if(values.email.length > 0 && values.email.indexOf('@') === -1) {
+    errors.email = 'Invalid Email: use @'
+  }
+  return errors;
 }
 
 export default reduxForm({
